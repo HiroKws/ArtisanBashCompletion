032af2b1eb57a9f33fac45da6d90ea79f14cfd48
Hirohisa Kawase <hirokws@gmail.com>
2014-08-06 21:25:20 +0900

    Added completion facility for some options

--- artisan.sh
+++ artisan.sh
@@ -12,7 +12,7 @@ _artisan_module()
         return 0;
     fi
 
-    local cur prev artisan_options
+    local cur prev artisan_options token
 
     # Ignore ':' and '-' from $COMP_WORDBREAKS then take last entered string.
     # 単語区切りの文字から':'と'-'を除外し、最後の入力文字列を取得する
@@ -20,14 +20,51 @@ _artisan_module()
 
     COMPREPLY=()
 
-    # Get commands and options
-    # コマンド、サブコマンド、オプションの取得
-    artisan_options=$(php artisan --no-ansi | awk '/^  [\-a-z]+/ { print $1 }; $2 ~/^-/ { gsub(/\|/, " -", $2); print $2 }')
+    # Completion for "--option=" type option.
+    # "--option="タイプのオプション対応
+    if [ "${cur}" = "=" ] || [ "${prev}" = "=" ]
+    then
+        if [ "${cur}" = "=" ]
+        then
+            token="${prev}"
+            cur=""
+        else
+            token="${COMP_WORDS[$COMP_CWORD-2]}"
+        fi
 
-    # For command options. If not want it, delete 'if' block.
-    # コマンドのオプション。必要ない場合は、if〜fiブロック間を削除する
-    if [ "${COMP_WORDS[2]}" = ":" ] && [ $COMP_CWORD -ge 4 ] ||
-        [ "${COMP_WORDS[2]}" != ":" ] && [ $COMP_CWORD -ge 2 ]
+        case "${token}" in
+            # Please change completions for "--env=" option freely.
+            # "-env="に対する候補、お好きなものに変更してください
+            "--env" )
+            COMPREPLY=($(compgen -W "local production testing" -- "${cur}"))
+            return 0
+            ;;
+            # Complete files list for "--path=".
+            # "--path"には、ファイルリストを候補にする
+            "--path" )
+            _filedir
+            return 0
+            ;;
+            # Complete directories
+            # ディレクトリ名の補完
+            "--dir" | "--resources" )
+            _filedir -d
+            return 0
+            ;;
+            # Complete database connection
+            # データベース接続名の補完
+            "--database" )
+            local databases=$(find . -name "database.php" | xargs -i php -r "\$db=include '{}'; if (key_exists('connections', \$db)) {foreach(\$db['connections'] as \$key=>\$data) echo \$key.' ';}" | tr ' ' "\n" | sort | uniq)
+            COMPREPLY=($(compgen -W "${databases}" -- "${cur}"))
+            return 0;
+            ;;
+        esac
+    fi
+
+    # For command options.
+    # コマンドのオプション。
+    if ( [ $COMP_CWORD -ge 4 ] && [ "${COMP_WORDS[2]}" = ":" ] ) ||
+       ( [ $COMP_CWORD -ge 2 ] && [ "${COMP_WORDS[2]}" != ":" ] )
     then
         if [ "${COMP_WORDS[2]}" = ":" ]
         then
@@ -36,6 +73,10 @@ _artisan_module()
             comm="${COMP_WORDS[1]}"
         fi
         artisan_options=$(php artisan "${comm}" --no-ansi --help | awk -e '/^ --/ { print $1 } $2 ~/^\(/ { gsub(/[\(\)]/, "", $2); gsub(/\|/, " -", $2); print $2}')
+    else
+        # Complete Artisan command names.
+        # Artisanコマンド名の補完
+        artisan_options=$(php artisan --no-ansi | awk '/^  [\-a-z]+/ { print $1 }; $2 ~/^-/ { gsub(/\|/, " -", $2); print $2 }')
     fi
 
     COMPREPLY=($(compgen -W "${artisan_options}" -- "${cur}"))
